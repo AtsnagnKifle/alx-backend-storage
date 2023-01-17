0a668a7de8118e386b5509a08056a77b5ea2c56e
atsnagnkifle <kifleatsnagn@gmail.com>
2023-01-18 00:01:19 +0300

    mongodn python

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''
+    task 10's module
+'''
+
+
+def update_topics(mongo_collection, name, topics):
+    '''
+        changes all topics of a collection's document based on the name
+    '''
+    mongo_collection.update_many(
+        {'name': name},
+        {'$set': {'topics': topics}}
+    )
